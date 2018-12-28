5281a84acd4c59076bf2a14bfea4494e16e54bbf
Risman <rsmnznr@gmail.com>
2018-12-29 00:32:24 +0700

    learn-redux

---
+++ client/src/redux/actions/alert.actions.js
@@ -0,0 +1,17 @@
+import { alertConstants } from '../constants';
+
+export const alertActions = {
+	success, error, clear
+}
+
+const success = (message) => {
+	return { type: alertConstants.SUCCESS, message }	
+}
+
+const error = (message) => {
+	return { type: alertConstants.ERROR, message }
+}
+
+const clear = () => {
+	return { type: alertConstants.clear }
+}
---
+++ client/src/redux/actions/user.actions.js
@@ -0,0 +1,85 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unreachable */
+import { userConstants } from  '../constants';
+import { userServices } from '../services';
+import { alertActions } from '.';
+import { history } from '../helpers';
+
+export const userActions = {
+	login, logout, getAll
+}
+
+const login = (email, password) => {
+	return dispatch => {
+		dispatch(request.emai({ email }));
+
+		userServices.login(email, password)
+			.then(
+				user => {
+					dispatch(success(user));
+					history.push('/');
+				},
+				error => {
+					dispatch(failure(error));
+					dispatch(alertActions.error(error));
+				}
+			);
+	};
+
+	const request = (user) => { 
+		return {
+			type: userConstants.LOGIN_REQUEST,
+			user
+		} 
+	}
+	const success = (user) => {
+		return {
+			type: userConstants.LOGIN_SUCCESS,
+			user
+		}
+	}
+	const failure = (error) => {
+		return {
+			type: userConstants.LOGIN_FAILURE,
+			error
+		}
+	}
+}
+
+const logout = () => {
+	userServices.logout();
+	return { type: userConstants.LOGOUT };
+}
+
+const getAll = () => {
+	return dispatch => {
+		dispatch(request());
+
+		userServices.getAll()
+			.then(
+				users => dispatch(success(users)),
+				error => {
+					dispatch(failure(error));
+					dispatch(alertActions.error(error));
+				}
+			);
+	};
+
+	const request = () => {
+		return {
+			type: userConstants.GETALL_REQUEST
+		}
+	}
+	const success = (users) => {
+		return { 
+			type: userConstants.GETALL_SUCCESS,
+			users
+		}
+	}
+	const failure = (error) => {
+		return {
+			type: userConstants.GETALL_FAILURE,
+			error
+		}
+	}
+}
